ae15b782281d76a82f4ce63009b5462c01964d8d
Rubal <rubalsingla19@gmail.com>
2022-10-06 12:18:38 +0530

    new commit by ravi

--- admin/availability.php
+++ admin/availability.php
@@ -2,7 +2,7 @@
 
 function availability() 
 {
-    /*testing */
+    /*New Code Push By Ravi */
     global $wpdb;
     $Bikes = $wpdb->prefix.'posts';
     $getbikes = $wpdb->get_results("Select * from $Bikes where post_type = 'motorcycle' and post_status='publish'");
